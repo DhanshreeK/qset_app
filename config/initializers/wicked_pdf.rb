f339899c045f97a9be5fe3f3ee3a8857e3849ac8
DhanshreeK <dhanshree.j.kamble@gmail.com>
2017-12-11 09:59:53 +0530

    added path to wicked pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,11 @@
 WickedPdf.config = {
   exe_path: '/usr/local/bin/wkhtmltopdf'
 }
+
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+else
+  wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"            
+end
+ 
+WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
